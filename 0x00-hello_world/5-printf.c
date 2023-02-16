cef9f767b50bf7e165f15b68e3067de651582b34
Jspartan89 <ebentaylor@hotmail.com>
2023-02-16 03:19:38 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-printf("with proper grammar, but the outcome is a piece of art,")
+#include <stdio.h>
+
+/**
+ *
+ */
+int main()
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+
+		return()
+}
