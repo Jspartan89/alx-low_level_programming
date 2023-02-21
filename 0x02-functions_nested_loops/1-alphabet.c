6a02730ce317adfedee407d64c2466e707e92e24
Jspartan89 <ebentaylor@hotmail.com>
2023-02-21 03:45:22 -0800

    0 put 1 alpha 2 alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -  prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
