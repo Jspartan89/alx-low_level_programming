fd39fe3f63fe47dfdd6e63cdda653b6283100d61
Jspartan89 <ebentaylor@hotmail.com>
2023-02-27 15:13:39 -0800

    7-puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * puts_half - Prints half of a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void puts_half(char *str)
+{
+	char ch;
+
+ch = arrayname[28];
+arrayname[28] = '\0';
+_putchar("%s", arrayname);
+arrayname[28] = ch;
+{
+	_putchar('\n');
+}
+}
